163f62f5fe4d0345748f23c3498ab52d39dbcb3a
Dmitriy Filatchev <o6jlako@mail.ru>
2020-07-28 11:38:44 +0300

    trying clone repo

--- prefixNext/pages/index.js
+++ prefixNext/pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div className="container">
-     trying git subtree
+     from cloning repo
     
 
       <style jsx>{`
